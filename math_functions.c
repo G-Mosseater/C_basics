ed7881f84e10334686484d156b3fd9b1e1f4ab82
Gabriel <Gabriel.bachelor@gmail.com>
2025-12-28 17:50:10 +0000

    mathFunctions

---
+++ math_functions.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+    float x = 45;
+
+    // x = sqrt(x);
+    // x= pow(x,2);
+    // x= round(x);
+    // x = floor(x);
+    // x = abs(x);
+    // x = log(x);
+    // x = sin(x);
+    // x = cos(x);
+    // x = tan(x);
+
+    printf("%.2f\n", x);
+
+    return 0;
+}
